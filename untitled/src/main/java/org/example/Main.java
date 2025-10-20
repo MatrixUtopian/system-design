294ff9a7c1fe66136ce525b878c7b788f6c6fe0a
MatrixUtopian <iec2019084@iiita.ac.in>
2025-10-20 14:57:41 +0530

    added locks files

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -4,14 +4,17 @@ import ThreadLearningClass.CustomRejectHandler;
 import ThreadLearningClass.CustomThreadFactory;
 import ThreadLearningClass.ThreadBasic;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
 //        Main.threadBasics();
-        Main.threadExecutor();
+//        Main.threadExecutor();
+//        Main.threadExecutorFuture();
+//        Main.threadExecutorCallable();
+        Main.threadSupplyAsync();
     }
 
     private static void threadBasics() {
@@ -38,5 +41,81 @@ public class Main {
             });
         }
         executor.shutdown();
+    }
+
+    private static void threadExecutorFuture() {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 2, 1,
+                TimeUnit.MINUTES, new ArrayBlockingQueue<>(2), new CustomThreadFactory(), new CustomRejectHandler());
+        Future<?> future =  executor.submit( () -> {
+            try {
+                Thread.sleep(7000);
+                System.out.println("Code Executed by thread: " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        System.out.println("Is done: " + future.isDone());
+
+        try {
+            future.get(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            System.out.println("Timeout Exception");
+        } catch (Exception e) {
+
+        }
+
+
+        try {
+            future.get();
+        } catch (Exception e) {
+            System.out.println("Error while waiting");
+        }
+
+        System.out.println("Is done: " + future.isDone());
+        System.out.println("Is cancelled: " + future.isCancelled());
+        executor.shutdown();
+    }
+
+    private static void threadExecutorCallable() {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 2, 1,
+                TimeUnit.MINUTES, new ArrayBlockingQueue<>(2), new CustomThreadFactory(), new CustomRejectHandler());
+        // Runnable doesn't return anything
+        Future<?> future =  executor.submit( () -> {
+                    System.out.println("Code Executed by thread: " + Thread.currentThread().getName());
+                });
+        // Callable returns a value
+        Future<Integer> callable =  executor.submit( () -> {
+            System.out.println("Code Executed by thread: " + Thread.currentThread().getName());
+            return 10;
+        });
+        System.out.println("Callable: " + callable.toString());
+        executor.shutdown();
+    }
+
+    private static void threadSupplyAsync() {
+
+        CompletableFuture<List<Integer>> completableFuture = CompletableFuture.supplyAsync(() -> {
+            List<Integer> list = new ArrayList<>();
+            System.out.println("Code Executed by thread: " + Thread.currentThread().getName());
+            for(int i = 1 ; i < 101; i++) {
+                list.add(i);
+            }
+            return list;
+        }).thenComposeAsync( list -> CompletableFuture.supplyAsync(() -> {
+            System.out.println("Code Executed by thread: " + Thread.currentThread().getName());
+                for(int i = 101 ; i < 201; i++) {
+                    list.add(i);
+                }
+                return list;
+        }));
+
+        try {
+            System.out.println(completableFuture.get());
+        } catch (Exception e) {
+
+        }
     }
 }
